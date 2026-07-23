5a56c56a9cc8d1232bf1594811584dbf6ffa4e7b
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-23 22:25:27 +0530

    updated code

--- frontend/src/components/farmer/FarmerSidebar/FarmerSidebar.jsx
+++ frontend/src/components/farmer/FarmerSidebar/FarmerSidebar.jsx
@@ -1,51 +1,53 @@
 import "./FarmerSidebar.css";
-import { Link } from "react-router-dom";
-import { FaPlusCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import {
   FaHome,
   FaSeedling,
   FaShoppingBasket,
   FaWallet,
   FaUser,
+  FaPlusCircle,
 } from "react-icons/fa";
 
 const FarmerSidebar = () => {
   return (
     <aside className="farmer-sidebar">
 
-      <h2>🌾 CropBridge</h2>
+      <div className="sidebar-logo">
+        🌾 <span>CropBridge</span>
+      </div>
 
       <nav>
 
-        <Link to="/farmer">
+        <NavLink to="/farmer" end>
           <FaHome />
           <span>Dashboard</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/farmer/my-crops">
+        <NavLink to="/farmer/my-crops">
           <FaSeedling />
           <span>My Crops</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/farmer/orders">
+        <NavLink to="/farmer/add-crop">
+          <FaPlusCircle />
+          <span>Add Crop</span>
+        </NavLink>
+
+        <NavLink to="/farmer/orders">
           <FaShoppingBasket />
           <span>Orders</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/farmer/payments">
+        <NavLink to="/farmer/payments">
           <FaWallet />
           <span>Payments</span>
-        </Link>
+        </NavLink>
 
-        <Link to="/farmer/profile">
+        <NavLink to="/farmer/profile">
           <FaUser />
           <span>Profile</span>
-        </Link>
-
-        <Link to="/farmer/add-crop">
-        <FaPlusCircle />
-        <span>Add Crop</span>
-        </Link>
+        </NavLink>
 
       </nav>
 
