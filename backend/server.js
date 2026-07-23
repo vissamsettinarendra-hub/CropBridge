a838c11e8a42c68d9203879d72d54c4258fad3de
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-23 22:20:29 +0530

    updated code

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173", // React frontend
+    credentials: true,               // Allow cookies
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🌾 CropBridge Backend Running Successfully",
+  });
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
