c45a6b714f00a0328eea5274b9a2e5243f6fa222
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-08-10 22:58:10 +0530

    initial updated code.

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -12,24 +12,13 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// =============================
-// Factory Routes
-// =============================
-
-// Send Request
+// Factory
 router.post("/", protect, sendRequest);
-
-// View Factory Requests
 router.get("/factory", protect, getFactoryRequests);
 
-// =============================
-// Farmer Routes
-// =============================
-
-// View Farmer Requests
+// Farmer
 router.get("/farmer", protect, getFarmerRequests);
-// Accept Request
 router.put("/:id/accept", protect, acceptRequest);
-// Reject Request
 router.put("/:id/reject", protect, rejectRequest);
-export default router ;
+
+export default router;
