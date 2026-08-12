73594d1994151278ff211a532a01dd0bdbefe523
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-08-12 20:04:58 +0530

    initial updated code.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,8 @@
 import User from "../models/User.js";
 import generateToken from "../utils/generateToken.js";
-
 // ======================================
 // Register User
 // ======================================
-
 export const registerUser = async (req, res) => {
   try {
    const { name, email, password, phone, role } = req.body;
