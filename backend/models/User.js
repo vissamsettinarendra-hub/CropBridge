082281d9c8882aca7802f2c6644e80d6590f0bad
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-08-22 23:38:24 +0530

    initial updated code..

--- backend/models/User.js
+++ backend/models/User.js
@@ -72,4 +72,4 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     this.password
   );
 };
-export default mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);   
